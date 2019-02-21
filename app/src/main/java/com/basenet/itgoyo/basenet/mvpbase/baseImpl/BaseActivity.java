fe7cab94f00dca4caa63156001c762cbeb456b3a
itgoyo <itgoyo@foxmail.com>
2019-02-21 14:14:45 +0800

    Initial commit

---
+++ app/src/main/java/com/basenet/itgoyo/basenet/mvpbase/baseImpl/BaseActivity.java
@@ -0,0 +1,108 @@
+package com.basenet.itgoyo.basenet.mvpbase.baseImpl;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.basenet.itgoyo.basenet.R;
+import com.basenet.itgoyo.basenet.mvpbase.BasePresenter;
+import com.basenet.itgoyo.basenet.mvpbase.BaseView;
+import com.basenet.itgoyo.basenet.utils.ActivityManager;
+import com.gyf.barlibrary.ImmersionBar;
+
+/*
+ * 项目名:    BaseLib
+ * 包名       com.zhon.baselib.mvpbase.baseImpl
+ * 文件名:    BaseActivity
+ * 创建者:    ZJB
+ * 创建时间:  2017/9/7 on 14:17
+ * 描述:     TODO 基类Activity
+ */
+public abstract class BaseActivity<P extends BasePresenter> extends AppCompatActivity
+        implements BaseView {
+
+    protected P presenter;
+    public Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+//        StatusBarUtil.setStatusBar(this);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            setTranslucentStatus(true);
+//        }
+//
+//        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+//        tintManager.setStatusBarTintEnabled(true);
+//        //设置状态栏颜色
+
+//        tintManager.setStatusBarTintResource(R.color.statusbar_bg);
+
+        ImmersionBar.with(this).statusBarColor(R.color.write_bg).statusBarDarkFont(true,0.2f)
+                .fitsSystemWindows(true)
+                .init();
+
+        ActivityManager.getAppInstance().addActivity(this);//将当前activity添加进入管理栈
+        presenter = initPresenter();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        ActivityManager.getAppInstance().removeActivity(this);//将当前activity移除管理栈
+        if (presenter != null) {
+            presenter.detach();//在presenter中解绑释放view
+            presenter = null;
+        }
+        super.onDestroy();
+    }
+
+
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    /**
+     * 在子类中初始化对应的presenter
+     *
+     * @return 相应的presenter
+     */
+    public abstract P initPresenter();
+
+
+    @Override
+    public void dismissLoadingDialog() {
+
+    }
+
+    @Override
+    public void showLoadingDialog(String msg) {
+
+    }
+
+
+    protected void exitApp() {
+        ActivityManager.getAppInstance().removeActivity(this);//将当前activity移除管理栈
+        if (presenter != null) {
+            presenter.detach();//在presenter中解绑释放view
+            presenter = null;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/basenet/itgoyo/basenet/retroft/BaseApiImpl.java
@@ -0,0 +1,198 @@
+package com.basenet.itgoyo.basenet.retroft;
+
+import android.util.Log;
+
+import com.basenet.itgoyo.basenet.utils.DiyLoggingInterceptor;
+import com.google.gson.GsonBuilder;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+/*
+ * 项目名:    BaseFrame
+ * 包名       com.zhon.baselib.retroft
+ * 文件名:    BaseApiImpl
+ * 创建者:    ZJB
+ * 创建时间:  2017/9/7 on 10:12
+ * 描述:     TODO
+ */
+public class BaseApiImpl implements BaseApi {
+    private volatile static Retrofit retrofit = null;
+    protected Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
+//    protected OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder();
+    protected OkHttpClient.Builder httpBuilder = getUnsafeOkHttpClient().newBuilder();
+
+    public BaseApiImpl(String baseUrl) {
+
+        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        retrofitBuilder.addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
+                        .setLenient()
+                        .create()
+                ))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(httpBuilder.addInterceptor(new DiyLoggingInterceptor().setLevel(DiyLoggingInterceptor.Level.BODY)).build())
+
+
+                /*.client(httpBuilder.addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        //获得请求信息，此处如有需要可以添加headers信息
+                        Request request = chain.request();
+                        //添加Cookie信息
+                        request.newBuilder().addHeader("Cookie","aaaa");
+                        //打印请求信息
+                        syso("url:" + request.url());
+                        syso("method:" + request.method());
+                        syso("request-body:" + request.body());
+
+                        //记录请求耗时
+                        long startNs = System.nanoTime();
+                        okhttp3.Response response;
+                        try {
+                            //发送请求，获得相应，
+                            response = chain.proceed(request);
+                        } catch (Exception e) {
+                            throw e;
+                        }
+                        long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
+                        //打印请求耗时
+                        syso("耗时:"+tookMs+"ms");
+                        //使用response获得headers(),可以更新本地Cookie。
+                        syso("headers==========");
+                        Headers headers = response.headers();
+                        syso(headers.toString());
+
+                        //获得返回的body，注意此处不要使用responseBody.string()获取返回数据，原因在于这个方法会消耗返回结果的数据(buffer)
+                        ResponseBody responseBody = response.body();
+
+                        //为了不消耗buffer，我们这里使用source先获得buffer对象，然后clone()后使用
+                        BufferedSource source = responseBody.source();
+                        source.request(Long.MAX_VALUE); // Buffer the entire body.
+                        //获得返回的数据
+                        Buffer buffer = source.buffer();
+                        //使用前clone()下，避免直接消耗
+                        syso("response:" + buffer.clone().readString(Charset.forName("UTF-8")));
+                        return response;
+                    }
+                }).build())*/
+//                .client(httpBuilder.addInterceptor(getLoggerInterceptor()).build())
+                .baseUrl(baseUrl);
+    }
+
+    private static void syso(String msg) {
+        System.out.println(msg);
+    }
+
+
+
+    /**
+     * 构建retroft
+     *
+     * @return Retrofit对象
+     */
+    @Override
+    public Retrofit getRetrofit() {
+        if (retrofit == null) {
+            //锁定代码块
+            synchronized (BaseApiImpl.class) {
+                if (retrofit == null) {
+                    retrofit = retrofitBuilder.build(); //创建retrofit对象
+                }
+            }
+        }
+        return retrofit;
+
+    }
+
+
+    @Override
+    public OkHttpClient.Builder setInterceptor(Interceptor interceptor) {
+        return httpBuilder.addInterceptor(interceptor);
+    }
+
+    @Override
+    public Retrofit.Builder setConverterFactory(Converter.Factory factory) {
+        return retrofitBuilder.addConverterFactory(factory);
+    }
+
+    /**
+     * 日志拦截器
+     * 将你访问的接口信息
+     *
+     * @return 拦截器
+     */
+    public HttpLoggingInterceptor getLoggerInterceptor() {
+        //日志显示级别
+        HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.HEADERS;
+        //新建log拦截器
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Log.d("ApiUrl", "--->" + message);
+            }
+        });
+        loggingInterceptor.setLevel(level);
+        return loggingInterceptor;
+    }
+
+    /**
+     *  忽略证书
+     * @return 访问Https
+     */
+    public static OkHttpClient getUnsafeOkHttpClient() {
+
+        try {
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            final javax.net.ssl.SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory);
+
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            return builder.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
